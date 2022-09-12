74d90ce248388f6ab95efcea121aec1c3c782e87
Vinícius Bonemer <v.bonemer@gmail.com>
2022-09-12 16:58:43 -0300

    Update searchagent for interactive use

--- src/searchagent.py
+++ src/searchagent.py
@@ -31,6 +31,9 @@ class SearchAgent(typing.Generic[T]):
     '''Implementation of a generic A* search algorithm.
     
     This implementation can be customized through dependency injection on the initializer.
+    The `get_best_path` method will return the best path, while the interactive version
+    `get_best_path_generator` will return a generator that will return the next potential
+    neighbor under consideration by the algorithm. 
     Note: The cost to reach the first element is always zero. The cost to reach any 
     neighbor of a given node N is equal to the cost of N returned by the cost function.
     '''
@@ -56,8 +59,10 @@ class SearchAgent(typing.Generic[T]):
         self.get_cost = cost_function
         self.heuristic = heuristic_function
         self.expand_neighbors = expansion_function
+        self.current_node: typing.Optional[Node[T]] = None
+        self.possible_next_node: typing.Optional[Node[T]] = None
 
-    def _reconstruct_path(self, destination: Node[T]) -> typing.List[T]:
+    def reconstruct_path(self, destination: Node[T]) -> typing.List[T]:
         path: typing.List[T] = [destination.wrapped]
         current = destination
         while current.previous_node is not None:
@@ -66,7 +71,7 @@ class SearchAgent(typing.Generic[T]):
         path.reverse()
         return path
 
-    def find_path(self, origin: T, goal: T) -> typing.List[T]:
+    def _find_path(self, origin: T, goal: T, interactive: bool) -> typing.Generator[Node[T], None, typing.List[T]]:
         wrapped_values: typing.Dict[T, Node[T]] = dict()
         start_heuristic = self.heuristic(origin, goal)
         start_node = Node(origin, 0, start_heuristic)
@@ -74,12 +79,12 @@ class SearchAgent(typing.Generic[T]):
         self.frontier.push(start_node, start_node.estimated_cost)
 
         while not self.frontier.is_empty():
-            current_node = self.frontier.pop()
-            if current_node.wrapped == goal:
-                return self._reconstruct_path(current_node)
-            for new_neighbor in self.expand_neighbors(current_node.wrapped):
+            self.current_node = self.frontier.pop()
+            if self.current_node.wrapped == goal:
+                return self.reconstruct_path(self.current_node)
+            for new_neighbor in self.expand_neighbors(self.current_node.wrapped):
                 # The cost of a path is sum {[origin, end)} (not including the end node)
-                cost_from_here = current_node.accumulated_cost + self.get_cost(current_node.wrapped)
+                cost_from_here = self.current_node.accumulated_cost + self.get_cost(self.current_node.wrapped)
                 new_neighbor_node = wrapped_values.get(new_neighbor, None)
 
                 if new_neighbor_node is None:
@@ -93,16 +98,82 @@ class SearchAgent(typing.Generic[T]):
                     new_neighbor_node.accumulated_cost = cost_from_here
                     new_estimation = self.heuristic(new_neighbor, goal)
                     new_neighbor_node.estimated_cost = cost_from_here + new_estimation
-                    new_neighbor_node.previous_node = current_node
+                    new_neighbor_node.previous_node = self.current_node
                     if new_neighbor_node not in self.frontier:
                         self.frontier.push(new_neighbor_node, new_neighbor_node.estimated_cost)
                     else:
                         # Changed cost of existing node in heap
                         self.frontier.update_priority(new_neighbor_node, new_neighbor_node.estimated_cost)
                         self.frontier.reorganize()
+                
+                if interactive:
+                    # For generators that get one trial at a time instead of the best path
+                    self.possible_next_node = new_neighbor_node
+                    yield self.possible_next_node
         
         # Failed to reach goal
         return []
 
+    def get_best_path(self, origin: T, goal: T) -> typing.List[T]:
+        '''Returns the best path from origin to goal'''
+        generator = self._find_path(origin, goal, interactive=False)
+        # Generator will have a single return if not interactive
+        try:
+            next(generator)
+        except StopIteration as e:
+            return e.value
+        raise RuntimeError('Unexpected generator state in `SearchAgent.get_best_path()`')
+    
+    def get_best_path_generator(self, origin: T, goal: T) -> typing.Generator[Node[T], None, typing.List[T]]:
+        '''Returns a generator that will interactively find the best path from `origin` to `goal`.
+        
+        At each call to `next`, the generator will return a Node under consideration as
+        the next one on the path. The current node that is being analyzed can be retrieved
+        under the `current_node` property and the next possible node under `possible_next_node`. 
+        To retrieve the path up to a given node, see `reconstruct_path`.
+        Example::
 
+            >>> sa = SearchAgent(
+            ...     cost_function=lambda x: x,
+            ...     heuristic_function=lambda x,y: y - x,
+            ...     expansion_function=lambda x: [x + 1]
+            ... )
+            >>> gen = sa.get_best_path_generator(0, 5)
+            >>> final_path = None
+            >>> while True:
+            ...     try:
+            ...         possible_next_node = next(gen)
+            ...         possible_path = sa.reconstruct_path(possible_next_node)
+            ...         print(f'{possible_next_node.wrapped}: {possible_path}')
+            ...     except StopIteration as e:
+            ...         final_path = e.value
+            ...         print(final_path)
+            ...         break
+            ... 
+                1: [0, 1]
+                2: [0, 1, 2]
+                3: [0, 1, 2, 3]
+                4: [0, 1, 2, 3, 4]
+                5: [0, 1, 2, 3, 4, 5]
+                [0, 1, 2, 3, 4 ,5]
+        '''
+        return self._find_path(origin, goal, interactive=True)
 
+if __name__ == '__main__':
+    # Example use
+    sa = SearchAgent(
+        cost_function=lambda x: x,
+        heuristic_function=lambda x,y: y - x,
+        expansion_function=lambda x: [x + 1]
+    )
+    gen = sa.get_best_path_generator(0, 5)
+    final_path = None
+    while True:
+        try:
+            possible_next_node = next(gen)
+            possible_path = sa.reconstruct_path(possible_next_node)
+            print(f'{possible_next_node.wrapped}: {possible_path}')
+        except StopIteration as e:
+            final_path = e.value
+            print(final_path)
+            break
--- tests/searchagent_test.py
+++ tests/searchagent_test.py
@@ -73,22 +73,81 @@ class SearchAgentTestCase(unittest.TestCase):
         correct_path: typing.List[WrappedType] = [
             (1,1), (1,2), (1,3), (2,3), (3,3), (3,4), (4,4), (5,4), (5,5)
         ]
-        path = self.search_agent.find_path((1, 1), (5, 5))
+        path_gen = self.search_agent._find_path((1, 1), (5, 5), interactive=False)
+        with self.assertRaises(StopIteration) as context_manager:
+            next(path_gen)
+        path = context_manager.exception.value
+        self.assertListEqual(path, correct_path)
+
+    def test_get_best_path(self) -> None:
+        correct_path: typing.List[WrappedType] = [
+            (1,1), (1,2), (1,3), (2,3), (3,3), (3,4), (4,4), (5,4), (5,5)
+        ]
+        path = self.search_agent.get_best_path((1, 1), (5, 5))
         self.assertListEqual(path, correct_path)
     
-    def test_path_finding_alternative_1(self) -> None:
+    def test_get_best_path_alternative_1(self) -> None:
         self.board[4, 4] = 7
         correct_path: typing.List[WrappedType] = [
             (1,1), (1,2), (1,3), (2,3), (3,3), (3,2), (4,2), (5,2), (5,3), (5,4), (5,5)
         ]
-        path = self.search_agent.find_path((1, 1), (5, 5))
+        path = self.search_agent.get_best_path((1, 1), (5, 5))
         self.assertListEqual(path, correct_path)
     
-    def test_path_finding_alternative_2(self) -> None:
+    def test_get_best_path_alternative_2(self) -> None:
         self.board[4, 4] = 7
         self.board[4, 5] = 4
         correct_path: typing.List[WrappedType] = [
             (1,1), (1,2), (1,3), (2,3), (3,3), (3,4), (3,5), (4,5), (5,5)
         ]
-        path = self.search_agent.find_path((1, 1), (5, 5))
+        path = self.search_agent.get_best_path((1, 1), (5, 5))
         self.assertListEqual(path, correct_path)
+
+class InteractiveSearchAgentTestCase(unittest.TestCase):
+    # 1  3
+    # 2  0
+
+    def __init__(self, *args, **kwargs):
+        super(InteractiveSearchAgentTestCase, self).__init__(*args, **kwargs)
+        self.search_agent: typing.Optional[SearchAgent[WrappedType]]
+        self.board = typing.Optional[typing.Dict[WrappedType, int]]
+    
+    def setUp(self) -> None:
+        self.board = {
+            (1, 1): 1,  (1, 2): 3,
+            (2, 1): 2,  (2, 2): 0
+        }
+        def cost_f(case: WrappedType) -> float:
+            return self.board[case]
+        def h_f(start: WrappedType, end: WrappedType) -> float:
+            if start == end: return 0
+            return (end[0] - start[0]) + (end[1] - start[1]) + cost_f(start) - 1
+        def exp_f(case: WrappedType) -> typing.List[WrappedType]:
+            l = []
+            # Up
+            if case[0] == 2: l.append( (case[0] - 1, case[1]) )
+            # Left
+            if case[1] == 2: l.append( (case[0], case[1] - 1) )
+            # Right
+            if case[1] == 1: l.append( (case[0], case[1] + 1) )
+            # Down
+            if case[0] == 1: l.append( (case[0] + 1, case[1]) )
+            return l
+        self.search_agent = SearchAgent(cost_f, h_f, exp_f)
+
+    def test_interactive_path_finding(self) -> None:
+        path_generator = self.search_agent.get_best_path_generator((1, 1), (2, 2))
+        # Current = (1, 1)
+        self.assertEqual(next(path_generator).wrapped, (1, 2))
+        self.assertEqual(self.search_agent.current_node.wrapped, (1, 1))
+        self.assertEqual(next(path_generator).wrapped, (2, 1))
+        # Current = (2, 1)
+        self.assertEqual(next(path_generator).wrapped, (1, 1))
+        self.assertEqual(self.search_agent.current_node.wrapped, (2, 1))
+        self.assertEqual(next(path_generator).wrapped, (2, 2))
+        # Current = (2, 2)
+        with self.assertRaises(StopIteration) as cm:
+            next(path_generator)
+        self.assertEqual(self.search_agent.current_node.wrapped, (2, 2))
+        final_list = cm.exception.value
+        self.assertListEqual(final_list, [(1, 1), (2, 1), (2, 2)])
