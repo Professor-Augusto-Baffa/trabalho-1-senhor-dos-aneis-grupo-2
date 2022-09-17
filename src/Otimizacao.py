23f1e9e14bf4534f4f4b1faeb75f9677480c91b4
Rperoba <rodperoba@gmail.com>
2022-09-17 08:58:43 -0300

    Melhor organização do individuo
    Criado um .py para as variaveis do individuo e separação das classes Individuo e Otimização em dois arquivos diferentes

---
+++ src/Otimizacao.py
@@ -0,0 +1,11 @@
+class Otimizacao():
+
+    def __init__(self,qnt_individuos,taxa_de_mutacao):
+            self.qnt_individuos = qnt_individuos
+            self.taxa_de_mutacao = taxa_de_mutacao
+    
+   # def cria_populacao(self):
+   #     populacao = []
+   #     for i in range(self.qnt_individuos):
+   #         populacao.append(self.cria_individuo())
+   #     return populacao
--- src/SolucaoDeEtapas.py
+++ src/SolucaoDeEtapas.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import individuo_vars
 import random
 #recombinação -> não faço ideia de como fazer
 #mutação -> retirar um hobbit de uma etapa e colocar ele em uma etapa difierente
@@ -6,45 +7,16 @@ import random
 #usar criacionismo???
 class Individuo():
     
-    def __init__(self):
+    def __init__(self, etapas, hobbits):
 
-        self.etapas = {
-                        "etapa_2" : 10,
-                        "etapa_3" : 30,
-                        "etapa_4" : 60,
-                        "etapa_5" : 65,
-                        "etapa_6" : 70,
-                        "etapa_7" : 75,
-                        "etapa_8" : 80,
-                        "etapa_9" : 85,
-                        "etapa_A" : 90,
-                        "etapa_B" : 95,
-                        "etapa_C" : 100,
-                        "etapa_D" : 110,
-                        "etapa_E" : 120,
-                        "etapa_F" : 130,
-                        "etapa_G" : 140,
-                        "etapa_H" : 150,
-                      }
+        self.etapas = etapas
 
-        self.hobbits = self.cria_hobbits()
+        self.hobbits = hobbits
         self.individuo = self.cria_individuo() #A estrutura representando o individuo em si
         self.tempoGasto = self.calcula_tempo() #Individuo mais apto sera o com menor tempo
-        self.populacao = self.cria_populacao()
         #print(self.individuo)
         #print(self.hobbits)
         #print(self.tempoGasto)
-
-    @staticmethod
-    def cria_hobbits():
-        # Nome do hobbit, Agilidade , Usos
-        hobbits = [
-            ["Frodo", 1.5, 7],
-            ["Sam", 1.4, 7],
-            ["Merry", 1.3, 7],
-            ["Pippin", 1.2, 7]
-        ]
-        return hobbits
     
     def cria_individuo(self):
         distr = []
@@ -99,5 +71,5 @@ class Otimizacao():
    #     return populacao
 
 if __name__ == '__main__':
-    Oti = Otimizacao(qnt_individuos=20,taxa_de_mutacao=1/16)
+    Individuo = Individuo(etapas=individuo_vars.etapas, hobbits=individuo_vars.hobbits)
     print("Terminei!")
