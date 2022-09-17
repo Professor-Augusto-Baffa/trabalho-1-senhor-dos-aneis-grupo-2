95cc0bde1d1ccd5753e2daa53859b12c3322e22a
Rperoba <rodperoba@gmail.com>
2022-09-17 09:46:20 -0300

    Criada classe otimização
    Otimização possui agora a criação de uma população de vários individuos.
    Nome do arquivo solução de etapas mudado para Individuo

--- src/Individuo.py
+++ src/Individuo.py
@@ -57,19 +57,6 @@ class Individuo():
             tempoTotal+=tempoGasto
         return tempoTotal
 
-
-class Otimizacao():
-
-    def __init__(self,qnt_individuos,taxa_de_mutacao):
-            self.qnt_individuos = qnt_individuos
-            self.taxa_de_mutacao = taxa_de_mutacao
-    
-   # def cria_populacao(self):
-   #     populacao = []
-   #     for i in range(self.qnt_individuos):
-   #         populacao.append(self.cria_individuo())
-   #     return populacao
-
 if __name__ == '__main__':
     Individuo = Individuo(etapas=individuo_vars.etapas, hobbits=individuo_vars.hobbits)
     print("Terminei!")
--- src/Otimizacao.py
+++ src/Otimizacao.py
@@ -1,11 +1,25 @@
+from Individuo import Individuo
+from individuo_vars import *
+
+from copy import deepcopy
+
+
 class Otimizacao():
 
     def __init__(self,qnt_individuos,taxa_de_mutacao):
+            
             self.qnt_individuos = qnt_individuos
             self.taxa_de_mutacao = taxa_de_mutacao
+            
+            self.populacao = self.cria_populacao()
+
     
-   # def cria_populacao(self):
-   #     populacao = []
-   #     for i in range(self.qnt_individuos):
-   #         populacao.append(self.cria_individuo())
-   #     return populacao
+    def cria_populacao(self):
+        populacao = []
+        for i in range(self.qnt_individuos):
+            populacao.append(Individuo(etapas=etapas,hobbits=deepcopy(hobbits)))
+        return populacao
+
+if __name__ == '__main__':
+    Otimizacao=Otimizacao(qnt_individuos=20, taxa_de_mutacao=1/16)
+    print("Terminei")
